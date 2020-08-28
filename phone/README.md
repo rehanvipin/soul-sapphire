348c0690c28cf5feb9e1512ec07ea6a240ed1b68
rehanvipin <rehanvipin@outlook.com>
2020-08-29 00:11:35 +0530

    Include new exercise: phone number normalizer :new_moon:
    Update readme to include exercise.

---
+++ phone/README.md
@@ -0,0 +1,11 @@
+# Phone-number normalizer
+Normalizes phone number entries in a database.  
+Uses regexp and db operations to clean out the phone-number column.  
+For more details check [Description](Description.md)
+
+## Usage:
+!Note: This will not be useful unless you specify the database and table according to the specific format.
+1. `go get`
+2. `go run .`
+
+Done as a part of [Gophercises](https://gophercises.com/)
--- phone/main.go
+++ phone/main.go
@@ -2,19 +2,80 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"regexp"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "lakedragon:lakedragon@tcp(127.0.0.1:3306)/company")
+	var uname, pass, schema = "username", "password", "phonedb"
+	connection := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", uname, pass, schema)
+	db, err := sql.Open("mysql", connection)
+	check(err)
+	conerr := db.Ping()
+	check(conerr)
+	defer db.Close()
+	// DB is ready
+
+	var phnumber string
+	rows, err := db.Query("select * from phone_numbers")
+	check(err)
+	defer rows.Close()
+
+	// Display all data
+	var phnumbers = []string{}
+	for rows.Next() {
+		err := rows.Scan(&phnumber)
+		check(err)
+		phnumbers = append(phnumbers, phnumber)
+	}
+
+	// Clean the data
+	var cleaned = map[string]string{}
+	for _, number := range phnumbers {
+		cleaned[number] = clean(number)
+		fmt.Printf("%14s - %s\n", number, cleaned[number])
+	}
+
+	// Update the data
+	var phno string
+	var delstatement = `
+	DELETE FROM phone_numbers
+	WHERE phone_number = ?;`
+	var updatestatement = `
+	UPDATE phone_numbers
+	SET phone_number = ?
+	WHERE phone_number = ?;`
+	for k, v := range cleaned {
+		// Check if exists, skip
+		err := db.QueryRow("select * from phone_numbers where phone_number = ?", v).Scan(&phno)
+		if err == nil {
+			if k == v {
+				continue
+			}
+			fmt.Printf("Deleting %14s\n", k)
+			_, exerr := db.Exec(delstatement, k)
+			check(exerr)
+		} else if err == sql.ErrNoRows {
+			fmt.Printf("Updating %14s\n", k)
+			_, exerr := db.Exec(updatestatement, v, k)
+			check(exerr)
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
+func clean(phno string) string {
+	re := regexp.MustCompile("[\\(\\-\\)\\s]")
+	matched := re.ReplaceAllString(phno, "")
+	return matched
+}
+
+func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	conerr := db.Ping()
-	if conerr != nil {
-		log.Fatal(conerr)
-	}
-	defer db.Close()
 }
